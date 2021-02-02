6fd3c1b600200c0ff4f19d403084d27a1eeecbed
Manoj-14 <manumadhu1425@gmail.com>
2021-02-03 00:02:07 +0530

    Yearly payment feature added

--- loan-cal.js
+++ loan-cal.js
@@ -1,9 +1,9 @@
 const widthBtn = document.querySelector('.cal-amt');
 widthBtn.style.width = '17rem';
-document.querySelector('#loan-cal').addEventListener("submit",function(e){
-    document.querySelector('.loader').style.display="block";
+document.querySelector('#loan-cal').addEventListener("submit", function (e) {
+    document.querySelector('.loader').style.display = "block";
     document.querySelector('.result-row').style.display = "none";
-    setTimeout(calculate,2000);
+    setTimeout(calculate, 2000);
     e.preventDefault();
 });
 // document.getElementById('loan-cal').addEventListener('submit', calculate);
@@ -25,17 +25,15 @@ function calculate(e) {
         monthlyPayment.value = monthly.toFixed(2);
         totalPayment.value = (monthly * calculatedPayment).toFixed(2);
         totalIntrest.value = (monthly * calculatedPayment - principal).toFixed(2);
-
-        yearlyPayment.value= (monthly*12).toFixed(2);
-
+        yearlyPayment.value = (monthly * 12).toFixed(2);
         document.querySelector('.result-row').style.display = "block";
-        document.querySelector('.loader').style.display="none";
+        document.querySelector('.loader').style.display = "none";
     } else {
         document.querySelector('.card h4').style.display = 'block';
-        document.querySelector('.loader').style.display="none";
+        document.querySelector('.loader').style.display = "none";
         setTimeout(function () {
             document.querySelector('.card h4').style.display = 'none';
         }, 3000);
     }
-    
+
 }
